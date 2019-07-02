4f3c3f22dbdaa51ad26f4d30db4c274657acdcd7
Ar Nazeh <Ar.nazeh@gmail.com>
2019-07-02 19:21:57 +0300

    Create 1_Select-Name.sql

---
+++ 1_Select-Name.sql
@@ -0,0 +1,11 @@
+ /* 1-Find the country that start with Y */
+SELECT name FROM world
+  WHERE name LIKE 'Y%';
+
+/* 2-Find the countries that end with y */
+SELECT name FROM world
+  WHERE name LIKE '%y'
+
+/* 3-Find the countries that contain the letter x */
+SELECT name FROM world
+  WHERE name LIKE '%x%'
