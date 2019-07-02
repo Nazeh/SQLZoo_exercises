591b9a769b173501ceec1d8b0f5dd4800b28d4fd
Ar Nazeh <Ar.nazeh@gmail.com>
2019-07-02 20:46:31 +0300

    Update 1_Select-Name.sql

--- 1_Select-Name.sql
+++ 1_Select-Name.sql
@@ -45,3 +45,15 @@ SELECT name FROM world
 /* 12-Find the country where the capital is the country plus "City". */
 SELECT name FROM world
   WHERE capital LIKE concat(name, ' City');
+  
+/* 13-Find the capital and the name where the capital includes the name of the country. */
+SELECT capital, name FROM world
+ WHERE capital LIKE concat('%',name,'%');
+ 
+/* 14- Find the capital and the name where the capital is an extension of name of the country. */
+SELECT capital, name FROM world
+  WHERE capital LIKE concat(name, '_%')
+  
+/* 15- Show the name and the extension where the capital is an extension of name of the country. */
+SELECT name, REPLACE(capital, name, '') AS extension FROM world
+  WHERE capital LIKE concat(name, '_%')
