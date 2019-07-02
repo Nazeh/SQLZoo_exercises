268a1d68361bc22a007f90cdb3734bb8f2716ca3
Ar Nazeh <Ar.nazeh@gmail.com>
2019-07-02 20:54:52 +0300

    Update 2_Select-From-World.sql

--- 2_Select-From-World.sql
+++ 2_Select-From-World.sql
@@ -1,1 +1,38 @@
+/* 1-Just to observe the table */
 
+/* 2-Show the name for the countries that have a population of 
+   at least 200 million */
+
+/* 3-Give the name and the per capita GDP for those countries 
+   with a population of at least 200 million. */
+
+/* 4-Show the name and population in millions 
+   for the countries of the continent 'South America'. */
+
+/* 5-Show the name and population for France, Germany, Italy */
+
+/* 6-Show the countries which have a name that 
+   includes the word 'United' */
+
+/* 7-Show the countries 
+   that are big by area or big by population. */
+   
+/* 8-Show the countries that are big by area or 
+   big by population but not both */
+   
+/* 9-For South America show population in millions and 
+   GDP in billions both to 2 decimal places. */
+
+/* 10-show per-capita GDP for the trillion dollar 
+   countries to the nearest $1000. */
+
+/* 11-Show the name and capital where 
+   the name and the capital have 
+   the same number of characters. */
+
+/* 12-Show the name and the capital where the first letters of 
+   each match. Don't include countries 
+   where the name and the capital are the same word. */
+
+/* 13-Find the country that has all the vowels 
+   and no spaces in its name. */
