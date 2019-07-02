648d6b307297c5249ca4fd6fe2faa36a682d0530
Ar Nazeh <Ar.nazeh@gmail.com>
2019-07-02 20:21:42 +0300

    Update 1_Select-Name.sql

--- 1_Select-Name.sql
+++ 1_Select-Name.sql
@@ -1,4 +1,4 @@
- /* 1-Find the country that start with Y */
+/* 1-Find the country that start with Y */
 SELECT name FROM world
   WHERE name LIKE 'Y%';
 
@@ -9,3 +9,39 @@ SELECT name FROM world
 /* 3-Find the countries that contain the letter x */
 SELECT name FROM world
   WHERE name LIKE '%x%'
+
+/* 4-Find the countries that end with land */
+SELECT name FROM world
+  WHERE name LIKE '%land'
+
+/* 5-Find the countries that start with C and end with ia */
+SELECT name FROM world
+  WHERE name LIKE 'C%ia'
+
+/* 6-Find the country that has oo in the name */
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
+
+/* 7-Find the countries that have three or more a in the name */
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
+
+/* 8-Find the countries that have "t" as the second character.*/
+SELECT name FROM world
+ WHERE name LIKE '_t%'; 
+
+/* 9-Find the countries that have two "o" characters separated by two others.*/
+SELECT name FROM world
+  WHERE name LIKE '%o__o%';
+
+/* 10-Find the countries that have exactly four characters. */
+SELECT name FROM world
+  WHERE name LIKE '____';
+
+/* 11-Find the country where the name is the capital city. */
+SELECT name FROM world
+  WHERE name LIKE capital;
+
+/* 12-Find the country where the capital is the country plus "City". */
+SELECT name FROM world
+  WHERE capital LIKE concat(name, ' City');
